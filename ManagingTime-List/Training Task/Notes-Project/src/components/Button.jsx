3ba264b6dec6ac80be9ae1826877eb4ad74178c6
Shaden Duaibes <sally.daibes@outlook.com>
2024-09-16 14:39:38 +0300

    we add timer for delete button

--- ManagingTime-List/Training Task/Notes-Project/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-export default function Button({children,onClick}){
-    return(
-        <button 
-          className="mt-4 text-white py-2 rounded-lg hover:bg-opacity-90 transition-colors w-full"
-          style={{ backgroundColor: '#089da1' }} onClick={onClick}>
-          {children}
-        </button>
-    );
-}
